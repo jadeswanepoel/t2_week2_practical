ef3757fb2f31ab6f642cf12b71064b556e26c276
jadeswanepoel <125471787+jadeswanepoel@users.noreply.github.com>
2023-05-06 22:21:28 +0200

    one take

---
+++ js/index.js
@@ -0,0 +1,18 @@
+let tim = 13;
+let jane = 23;
+let john = 10;
+
+tim = tim + 10;
+
+let teamTotal = tim + jane;
+let victory = john / 2;
+let total = teamTotal + victory;
+
+
+
+let answer = "tim and jane now have " + total + " marbles";
+
+console.log(answer)
+
+
+
